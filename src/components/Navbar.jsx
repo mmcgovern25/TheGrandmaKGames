e4843ce70029107e3f48dfcb280244cc8d9f77cb
Matthew McGovern <mattmcg37@gmail.com>
2024-07-24 12:55:57 -0300

    navwork

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,18 @@ import logo from '../assets/grandmagames.png'
 
 const Navbar = () => {
   return (
-    <nav>
-        <img src={logo} alt="The Grandma Games" className="w-[125px] sm:w-[125px] md:w-[125px]"/>
-      <ul>
-      </ul>
+    <nav className='relative z-50 transition duration-700 bg-white'>
+
+      <div className='flex justify-between p-4'>
+          <img src={logo} alt="The Grandma Games" className="-ml-4 w-[125px] sm:w-[125px] md:w-[125px]"/>
+        <ul className="flex flex-row p-4 md:flex space-y-5 ml-8">
+          <li>About</li>
+          <li>Trivia</li>
+          <li>Connect 4</li>
+          <li>Memory Lane</li>
+          <li>Tik Tak Toe</li>
+        </ul>
+      </div>
     </nav>
   )
 }
