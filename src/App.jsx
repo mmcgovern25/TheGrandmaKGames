4406c597fdd2dca7216566e8e382f780d7801ffd
Matthew McGovern <mattmcg37@gmail.com>
2024-07-31 12:09:40 -0300

    body applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
-
-import Hero from './components/Hero'
+import Hero from './components/Hero';
 import About from './components/About';
 import './index.css';
 import FeaturedGames from './components/FeaturedGames';
-import { useState } from 'react';
-
-
-
+import { useOutletContext } from 'react-router-dom';
 
 function App() {
-
-
-  const [theme, setTheme] = useState('light')
+  const { theme } = useOutletContext();
 
   return (
     <div className={`hero ${theme}`}>
-      <Hero/>
-      <FeaturedGames/>
-      <About/>
+      <Hero />
+      <FeaturedGames />
+      <About />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,17 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Root() {
+  const [theme, setTheme] = useState('light');
 
-  const [theme, setTheme] = useState('light')
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
   return (
-  <>
-    <Navbar theme={theme} setTheme={setTheme}/>
-    <Outlet />
-  </>
-  )
+    <>
+      <Navbar theme={theme} setTheme={setTheme} />
+      <Outlet context={{ theme }} />
+    </>
+  );
 }
 
 export default Root;
