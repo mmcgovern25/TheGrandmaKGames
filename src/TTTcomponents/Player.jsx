cd0b36a1b3e9816b21738450779c83694b6f2487
Matthew McGovern <mattmcg37@gmail.com>
2024-08-20 07:31:39 -0300

    input field populating - css conflict

--- src/TTTcomponents/Player.jsx
+++ src/TTTcomponents/Player.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { useState } from 'react'
+import '../extraCSS/Tiktaktoe.css'
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+
+  const [playerName, setPlayerName] = useState(initialName);
   const [ isEditing, setIsEditing ] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(!isEditing);
+    setIsEditing((editing) => !editing);
   }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value)
+  }
+
 
   return (
     <li>
-        <span>
-          {isEditing ? <input type='text' required value={name}/> : <input type='text' required value={name}/> }
+        <span className="player">
+          <span className='player-name'>{isEditing ? <input type="text" required value={playerName} onChange={handleChange} style={{ display: 'block', visibility: 'visible', color: 'black', backgroundColor: 'white' }} // Force visibility
+          /> : <span className="player-name">{playerName}</span> }</span>
           <span className='player-symbol'>{symbol}</span>
           <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </span>
--- src/pages/TikTakToe.jsx
+++ src/pages/TikTakToe.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import Player from '../TTTcomponents/Player';
 import '../extraCSS/Tiktaktoe.css'
 
-const TikTakToe = () => {
+function TikTakToe() {
   return (
-    <>
+    <main>
       <div className='mt-20 flex flex-col justify-center items-center px-4'>
         <div className={`bruno-ace-sc-regular text-1xl sm:text-2xl md:text-3xl lg:text-4xl sm:mb-2 md:mb-4 `}>Tik Tak Toe</div>
       </div>
 
       <div id="game-container">
         <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+          <Player className="player-name" initialName="Player 1" symbol="X" />
+          <Player className="player-name" initialName="Player 2" symbol="O" />
         </ol>
+        GAME BOARD
       </div>
-
-
-  </>
+      LOG
+  </main>
   )
 }
 
